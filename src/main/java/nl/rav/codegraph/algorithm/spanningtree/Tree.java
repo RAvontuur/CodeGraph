37c62a939337e45d378f9cf9bf06796fd6806d72
Rene Avontuur <rene.avontuur@chello.nl>
2016-12-12 21:20:36 +0100

    added splitting of  a tree into a new Tree

--- src/main/java/nl/rav/codegraph/algorithm/spanningtree/Tree.java
+++ src/main/java/nl/rav/codegraph/algorithm/spanningtree/Tree.java
@@ -1,7 +1,10 @@
 package nl.rav.codegraph.algorithm.spanningtree;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by rene on 11-12-16.
@@ -11,6 +14,8 @@ public class Tree {
     private Edge rootEdge;
     private Set<Edge> edges = new HashSet<>();
     private Set<Long> nodes = new HashSet<>();
+    //edges that connect this tree to the roots of other trees
+    private Set<Edge> crossEdges = new HashSet<>();
 
     public Edge getRootEdge() {
         return rootEdge;
@@ -32,44 +37,84 @@ public class Tree {
         return edges.size();
     }
 
-    public boolean addEdge(Edge edge) {
-        if (edges.contains(edge)) {
-            return true;
-        }
+    /**
+     * @param edge
+     * @return list of modified or new trees
+     */
+    public List<Tree> addEdge(Edge edge) {
+        List<Tree> result = new ArrayList<>();
 
-        if (rootEdge == null) {
-            //first edge
-            rootEdge = edge;
-        } else if (!nodes.contains(edge.getFromId()) && !nodes.contains(edge.getToId())) {
-            //not any connection possible
-            return false;
-        } else if (rootEdge.getFromId() == edge.getToId()) {
+        if (isDuplicate(edge)) {
+            //do nothing
+        } else if (isRootEdge(edge)) {
             //new root
             rootEdge = edge;
-        } else if (nodes.contains(edge.getToId())) {
-            //cycle or forward
-            return addCycleOrForward(edge);
+            addEdgeAndNodes(result, edge);
+        } else if (isLibraryMakingEdge(edge)) {
+            makeLibrary(result, edge);
+        } else if (isIsolatedEdge(edge)) {
+            makeNewTree(result, edge);
+        } else {
+            addEdgeAndNodes(result, edge);
         }
 
+        return result;
+    }
+
+    private void addEdgeAndNodes(List<Tree> result, Edge edge) {
         edges.add(edge);
-
-        addNode(edge.getFromId());
-        addNode(edge.getToId());
-
-        return true;
+        nodes.add(edge.getFromId());
+        nodes.add(edge.getToId());
+        result.add(this);
     }
 
-    private boolean addCycleOrForward(Edge edge) {
-        if (!nodes.contains(edge.getFromId())) {
-            // edge belongs to a new tree
-            return false;
-        }
-
-
-        return true;
+    private boolean isDuplicate(Edge edge) {
+        return edges.contains(edge);
     }
 
-    private void addNode(long id) {
+    private boolean isRootEdge(Edge edge) {
+        return (rootEdge == null) ||
+                ((rootEdge.getFromId() == edge.getToId()) && (!nodes.contains(edge.getFromId())));
+    }
+
+    private boolean isIsolatedEdge(Edge edge) {
+        return !nodes.contains(edge.getFromId()) && !nodes.contains(edge.getToId());
+    }
+
+    private void makeNewTree(List<Tree> result, Edge edge) {
+        Tree tree = new Tree();
+        tree.addEdge(edge);
+        result.add(tree);
+    }
+
+    private boolean isLibraryMakingEdge(Edge edge) {
+        return nodes.contains(edge.getToId()) && (rootEdge != null) && (rootEdge.getFromId() != edge.getToId());
+    }
+
+    private void makeLibrary(List<Tree> result, Edge edge) {
+        result.add(splitTree(edge.getToId()));
+        //TODO add to crossEdges + new tree or attach to tree
+    }
+
+    private Tree splitTree(long id) {
+        Tree tree = new Tree();
+        transferEdges(tree, id);
+        //add removed root-node, as it is still a destination node
         nodes.add(id);
+        return tree;
+    }
+
+    private void transferEdges(Tree tree, long fromId) {
+        Set<Edge> edgesFound = findEdges(fromId);
+        edgesFound.stream().forEach(edge -> {
+            tree.addEdge(edge);
+            edges.remove(edge);
+            transferEdges(tree, edge.getToId());
+        });
+        nodes.remove(fromId);
+    }
+
+    private Set<Edge> findEdges(long fromId) {
+        return edges.stream().filter(edge -> edge.getFromId() == fromId).collect(Collectors.toSet());
     }
 }
--- src/test/java/nl/rav/test/codegraph/algorithm/TreeTest.java
+++ src/test/java/nl/rav/test/codegraph/algorithm/TreeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertFalse;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -22,13 +22,13 @@ public class TreeTest {
         Edge edge2 = new Edge(2, 3);
 
         Tree tree = new Tree();
-        assertTrue(tree.addEdge(edge1));
-        assertTrue(tree.addEdge(edge2));
+        assertThat(tree.addEdge(edge1).get(0), is(tree));
+        assertThat(tree.addEdge(edge2).get(0), is(tree));
 
         assertThat(tree.getRootEdge(), is(edge1));
 
-        assertThat(2, is(tree.edgeSize()));
-        assertThat(3, is(tree.nodeSize()));
+        assertThat(tree.edgeSize(), is(2));
+        assertThat(tree.nodeSize(), is(3));
 
         assertTrue(tree.containsEdge(edge1));
         assertTrue(tree.containsEdge(edge2));
@@ -46,13 +46,13 @@ public class TreeTest {
         Edge edge2 = new Edge(1, 2);
 
         Tree tree = new Tree();
-        assertTrue(tree.addEdge(edge1));
-        assertTrue(tree.addEdge(edge2));
+        assertThat(tree.addEdge(edge1).get(0), is(tree));
+        assertThat(tree.addEdge(edge2).get(0), is(tree));
 
         assertThat(tree.getRootEdge(), is(edge2));
 
-        assertThat(2, is(tree.edgeSize()));
-        assertThat(3, is(tree.nodeSize()));
+        assertThat(tree.edgeSize(), is(2));
+        assertThat(tree.nodeSize(), is(3));
 
         assertTrue(tree.containsEdge(edge1));
         assertTrue(tree.containsEdge(edge2));
@@ -70,13 +70,13 @@ public class TreeTest {
         Edge edge2 = new Edge(1, 2);
 
         Tree tree = new Tree();
-        assertTrue(tree.addEdge(edge1));
-        assertTrue(tree.addEdge(edge2));
+        assertThat(tree.addEdge(edge1).get(0), is(tree));
+        assertTrue(tree.addEdge(edge2).isEmpty());
 
         assertThat(tree.getRootEdge(), is(edge1));
 
-        assertThat(1, is(tree.edgeSize()));
-        assertThat(2, is(tree.nodeSize()));
+        assertThat(tree.edgeSize(), is(1));
+        assertThat(tree.nodeSize(), is(2));
 
         assertTrue(tree.containsEdge(edge1));
         assertTrue(tree.containsEdge(edge2));
@@ -87,24 +87,73 @@ public class TreeTest {
     }
 
     @Test
-    public void testNoConnection() {
+    public void testNewTree() {
 
         Edge edge1 = new Edge(1, 2);
         Edge edge2 = new Edge(3, 4);
 
         Tree tree = new Tree();
-        assertTrue(tree.addEdge(edge1));
-        assertFalse(tree.addEdge(edge2));
+        assertThat(tree.addEdge(edge1).get(0), is(tree));
+        Tree newTree = tree.addEdge(edge2).get(0);
+        assertThat(newTree, not(is(tree)));
 
         assertThat(tree.getRootEdge(), is(edge1));
 
-        assertThat(1, is(tree.edgeSize()));
-        assertThat(2, is(tree.nodeSize()));
+        assertThat(tree.edgeSize(), is(1));
+        assertThat(tree.nodeSize(), is(2));
 
         assertTrue(tree.containsEdge(edge1));
 
         assertTrue(tree.containsNode(1));
         assertTrue(tree.containsNode(2));
 
+        // new tree
+        assertThat(newTree.getRootEdge(), is(edge2));
+
+        assertThat(newTree.edgeSize(), is(1));
+        assertThat(newTree.nodeSize(), is(2));
+
+        assertTrue(newTree.containsEdge(edge2));
+
+        assertTrue(newTree.containsNode(3));
+        assertTrue(newTree.containsNode(4));
+    }
+
+    @Test
+    public void testNewLibraryTree() {
+
+        Edge edge1 = new Edge(1, 3);
+        Edge edge2 = new Edge(3, 4);
+        Edge edge3 = new Edge(2, 3);
+
+        Tree tree = new Tree();
+        assertThat(tree.addEdge(edge1).get(0), is(tree));
+        assertThat(tree.addEdge(edge2).get(0), is(tree));
+        Tree newTree = tree.addEdge(edge3).get(0);
+
+        // tree
+        assertThat(tree.getRootEdge(), is(edge1));
+
+        assertThat(tree.edgeSize(), is(1));
+        assertThat(tree.nodeSize(), is(2));
+
+        assertTrue(tree.containsEdge(edge1));
+
+        assertTrue(tree.containsNode(1));
+        assertTrue(tree.containsNode(3));
+
+        // new tree I
+        assertThat(newTree.getRootEdge(), is(edge2));
+
+        assertThat(newTree.edgeSize(), is(1));
+        assertThat(newTree.nodeSize(), is(2));
+
+        assertTrue(newTree.containsEdge(edge2));
+
+        assertTrue(newTree.containsNode(3));
+        assertTrue(newTree.containsNode(4));
+
+        // new tree II
+        // TODO
     }
 }
